88c6793c0fc807363e38fc193b38ef4f7b416ca1
Can Soylu <hsyncn17@gmail.com>
2021-05-29 19:10:55 +0300

    added cart context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartItems = (
-    <ul className="cart-items">
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
@@ -10,17 +11,19 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
-      {cartitems}
+    <Modal onClose={props.onClose}>
+      {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={classes.action}>
-        <button className={classes["button--alt"]}>Close</button>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
