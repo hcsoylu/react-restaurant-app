f57e62a75a26b79549d1670b424bf0b72086e1d6
Can Soylu <hsyncn17@gmail.com>
2021-05-30 21:26:21 +0300

    fixed some proplem in cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -58,6 +58,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (aciton === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -81,11 +85,18 @@ const CartProvider = (props) => {
     });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({
+      type: "CLEAR",
+    });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
